9c14c47053640630b06ba2f40d70f54858a52dbe
freeman <873040828@qq.com>
2024-05-24 13:06:04 +0800

    第二版，后面会补三个的扫描

---
+++ GUI_Main.py
@@ -0,0 +1,80 @@
+# 导入sys
+import os.path
+import sys
+import xmlword
+# 我们要展示一个普通的窗口，所以需要导入QWidget，用来让我们自己的类继承
+from PySide6.QtWidgets import QApplication, QMainWindow,QFileDialog
+# 导入我们生成的界面
+from ui_main import Ui_MainWindow
+from docx import Document
+import xlwt
+import xlrd
+
+# 继承QWidget类，以获取其属性和方法
+class MyWidget(QMainWindow):
+    #绑定槽使用
+    def start(self):
+        self.ui.selectFirstTitle.currentIndexChanged.connect(self.ChangeTextfirst)
+        self.ui.selectSecondTitle.currentIndexChanged.connect(self.ChangeTextSecond)
+        self.ui.selectThirdTitle.currentIndexChanged.connect(self.ChangeTextThird)
+        self.ui.fileBtn.clicked.connect(self.opnefile)
+        self.ui.pushButton_xml_header.clicked.connect(self.xmlinfoHeader)
+        self.ui.ExcuteResult.clicked.connect(self.Output)
+        print(self.doc_file_name)
+    def Output(self):
+        self.ui.textEdit.setText("正在生成中")
+        outputfilename="\\"+self.ui.ResultName.text()+".xls"
+        path=os.getcwd()+outputfilename
+        print(path)
+        whetherexist=os.path.exists(path)
+        if(whetherexist):
+            self.ui.textEdit.setText("文件已存在，请删除或重新命名！")
+            return
+        try:
+            self.xmlTool.Output(self.ui.lineEdit_first_title.text(),self.ui.lineEdit_second_title.text(),path)
+        except Exception as e:
+            self.ui.textEdit.setText("生成失败")
+            return
+        self.ui.textEdit.setText("生成成功")
+    def ChangeTextSecond(self):
+        self.ui.lineEdit_second_title.setText(self.ui.selectSecondTitle.currentText())
+    def ChangeTextThird(self):
+        self.ui.lineEdit_third_title.setText(self.ui.selectThirdTitle.currentText())
+    def ChangeTextfirst(self):
+        self.ui.lineEdit_first_title.setText(self.ui.selectFirstTitle.currentText())
+    #解析doc里的全部标题
+    def xmlinfoHeader(self):
+        self.xmlTool=xmlword.MyXmlFunc(self.doc_file_name)
+        result=self.xmlTool.xmlinfo()
+        if(result==-1):
+            self.ui.xml_title_info("请选择正确的doc文件！")
+        for i in result:
+            self.ui.xml_title_info.append(i)
+            print(i)
+            self.ui.selectFirstTitle.addItem(i)
+            self.ui.selectSecondTitle.addItem(i)
+            self.ui.selectThirdTitle.addItem(i)
+    def opnefile(self):
+        filePath=QFileDialog.getOpenFileName()
+        print(str(filePath[0]))
+        self.ui.lineEdit_FilePath.setText(filePath[0])
+        self.doc_file_name=str(filePath[0])
+    def __init__(self):
+        super().__init__()
+        self.doc_file_name=""
+        self.xmlTool=xmlword.MyXmlFunc("")
+        # 设置界面为我们生成的界面
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.start()
+# 程序入口
+if __name__ == "__main__":
+    # 初始化QApplication，界面展示要包含在QApplication初始化之后，结束之前
+    app = QApplication(sys.argv)
+
+    # 初始化并展示我们的界面组件
+    window = MyWidget()
+    window.show()
+
+    # 结束QApplication
+    sys.exit(app.exec_())
--- xmlword.py
+++ xmlword.py
@@ -1,61 +1,119 @@
 from docx import Document
 import xlwt
 import xlrd
-path = 'doc/M3接口分册20240508.docx'
-
-doc = Document(path)
 # 输出文件中的所有文字
 def extractTitle(paragraph,name):
 	if(paragraph.style.name==name):
 		return paragraph
 	else:
 		return None
-if __name__ == '__main__':
-	title=[]
-	second_title=[]
-	second_title_content=[]
-	first_title_name="附录二级条标题"
-	second_level_title_name="附录三级条标题"
+class MyXmlFunc:
+	def __init__(self,file_path):
+		self.file_path=file_path
 
-	for paragraph in doc.paragraphs:
-		# 判断该段落的标题级别
-		#print(paragraph.style.name)
-		# 这里用isTitle()临时代表，具体见下文介绍的方法
-		result=extractTitle(paragraph,first_title_name)
-		if(result!=None):
-			if(len(title)==0):
+	def Output(self,first_title_name,second_level_title_name,result_xlsx):
+		doc=Document(self.file_path)
+		title = []
+		second_title = []
+		second_title_content = []
+		# first_title_name = "附录二级条标题"
+		# second_level_title_name = "附录三级条标题"
+
+		for paragraph in doc.paragraphs:
+			# 判断该段落的标题级别
+			# print(paragraph.style.name)
+			# 这里用isTitle()临时代表，具体见下文介绍的方法
+			result = extractTitle(paragraph, first_title_name)
+			if (result != None):
+				if (len(title) == 0):
+					title.append(result.text)
+					continue
 				title.append(result.text)
-				continue
-			title.append(result.text)
-			second_title.append(second_title_content)
-			second_title_content=[]
-		result=extractTitle(paragraph,second_level_title_name)
-		if(result!=None):
-			second_title_content.append(result.text)
-	for i,j in zip(title,second_title):
-		print(i)
-		for index in j:
-			print("\t"+index)
-	#读取word文档完成，创建excel表格
+				second_title.append(second_title_content)
+				second_title_content = []
+			result = extractTitle(paragraph, second_level_title_name)
+			if (result != None):
+				second_title_content.append(result.text)
+		# for i, j in zip(title, second_title):
+		# 	print(i)
+		# 	for index in j:
+		# 		print("\t" + index)
+		# 读取word文档完成，创建excel表格
+		f2 = open(result_xlsx, "w+", encoding='utf-8')
+		df2 = xlwt.Workbook(f2)
+		table2 = df2.add_sheet('test')
+		table2.write(0, 0, "序号")
+		table2.write(0, 1, "模块")
+		table2.write(0, 2, "用例标题")
+		table2.write(0, 3, "测试结果")
+		line_begin = 1
+		line_end = 1
+		for i, j in zip(title, second_title):
+			line_end = line_begin + len(j)
+			table2.write_merge(line_begin, line_end, 1, 1, i)
+			for index in range(len(j)):
+				table2.write(line_begin + index, 2, j[index])
+			line_begin = line_end + 1
 
-	f2=open("result2.xls","w+",encoding='utf-8')
-	df2=xlwt.Workbook(f2)
-	table2 = df2.add_sheet('test')
-	table2.write(0,0,"序号")
-	table2.write(0,1,"模块")
-	table2.write(0, 2, "用例标题")
-	table2.write(0,3,"测试结果")
-	line_begin=1
-	line_end=1
-	for i,j in zip(title,second_title):
-		line_end=line_begin+len(j)
-		table2.write_merge(line_begin,line_end,1,1,i)
-		for index in range(len(j)):
-			table2.write(line_begin+index,2,j[index])
-		line_begin=line_end+1
-
-	df2.save("./result2.xls")
-
+		df2.save(result_xlsx)
+		f2.close()
+	def xmlinfo(self):
+		print(self.file_path)
+		try:
+			doc=Document(self.file_path)
+		except Exception as e:
+			return -1
+		titleset = set()
+		for paragraph in doc.paragraphs:
+			if(paragraph.style.name!="Normal"):
+				titleset.add(paragraph.style.name)
+		return titleset
+#
+# if __name__ == '__main__':
+# 	title=[]
+# 	second_title=[]
+# 	second_title_content=[]
+# 	first_title_name="附录二级条标题"
+# 	second_level_title_name="附录三级条标题"
+#
+# 	for paragraph in doc.paragraphs:
+# 		# 判断该段落的标题级别
+# 		#print(paragraph.style.name)
+# 		# 这里用isTitle()临时代表，具体见下文介绍的方法
+# 		result=extractTitle(paragraph,first_title_name)
+# 		if(result!=None):
+# 			if(len(title)==0):
+# 				title.append(result.text)
+# 				continue
+# 			title.append(result.text)
+# 			second_title.append(second_title_content)
+# 			second_title_content=[]
+# 		result=extractTitle(paragraph,second_level_title_name)
+# 		if(result!=None):
+# 			second_title_content.append(result.text)
+# 	for i,j in zip(title,second_title):
+# 		print(i)
+# 		for index in j:
+# 			print("\t"+index)
+# 	#读取word文档完成，创建excel表格
+# 	f2=open("result2.xls","w+",encoding='utf-8')
+# 	df2=xlwt.Workbook(f2)
+# 	table2 = df2.add_sheet('test')
+# 	table2.write(0,0,"序号")
+# 	table2.write(0,1,"模块")
+# 	table2.write(0, 2, "用例标题")
+# 	table2.write(0,3,"测试结果")
+# 	line_begin=1
+# 	line_end=1
+# 	for i,j in zip(title,second_title):
+# 		line_end=line_begin+len(j)
+# 		table2.write_merge(line_begin,line_end,1,1,i)
+# 		for index in range(len(j)):
+# 			table2.write(line_begin+index,2,j[index])
+# 		line_begin=line_end+1
+#
+# 	df2.save("./result2.xls")
+#
 
 # 摘要
 #
